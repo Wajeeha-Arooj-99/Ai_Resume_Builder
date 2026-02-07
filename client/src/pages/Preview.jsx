84f5d8c29a9978252d6b6a65f0c508c64beafd20
Wajeeha Arooj <135042860+Wajeeha-Arooj-99@users.noreply.github.com>
2026-02-07 23:20:10 +0500

    main

--- client/src/pages/Preview.jsx
+++ client/src/pages/Preview.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { dummyResumeData } from '../assets/assets'
 
 const Preview = () => {
+
+  const { resumeId } = useParams()
+
+  const [resumeData, setResumeData ] = useState(null)
+
+const loadResume = async () => {
+  setResumeData(dummyResumeData.find(resume => resume._id === resumeId || null))
+}
+
+useEffect (()=>{
+  loadResume()
+}, [])
   return (
     <div>
         <h1>Preview Page</h1>
