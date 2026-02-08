8c4216831dcf5d39683da380f558ecc0312a95e1
Wajeeha Arooj <135042860+Wajeeha-Arooj-99@users.noreply.github.com>
2026-02-08 18:04:36 +0500

    main

--- server/server.js
+++ server/server.js
@@ -1,11 +1,24 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import connectDB from "./configs/db.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
-const PORT = process.env.PORT || 300;
+const PORT = process.env.PORT || 3000;
 
-app.use(express.json())
-app.use(cors())
+// Database connection
+await connectDB();
 
-app.get('/', (req, res)=> res.send("Server is live..."))
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.get('/', (req, res) => res.send("Server is live..."));
+app.use('/api/users', userRouter);
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
